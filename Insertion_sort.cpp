9fba58c1b6d639a696e8bc2a1f8b84fb26a574a2
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-03-10 12:19:36 +0800

    menambahkan output hasil sorting

--- Insertion_sort.cpp
+++ Insertion_sort.cpp
@@ -53,3 +53,8 @@ void display (){
     cout << "\n==============";
     cout << "\nElement Array" << endl;
     cout << "\n==============" << endl;
+     for(int j = 0; j < n; j++){
+        cout << arr[j] << endl;
+    }
+    cout << endl;
+}
